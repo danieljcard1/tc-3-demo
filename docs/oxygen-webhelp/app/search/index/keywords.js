f60753d0988012450768d05e7c8462f7aea30eb9
danieljcard1 <danieljcard1@gmail.com>
2023-10-18 16:42:00 -0500

    initial project commit

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Fermentation",p:["p1"]},{w:"Process",p:["p1"]},{w:"in",p:["p1"]},{w:"Sourdough",p:["p1","p2","p3","p4","p5"]},{w:"Bread",p:["p1"]},{w:"Making",p:["p2","p3"]},{w:"a",p:["p2","p3"]},{w:"simple",p:["p2"]},{w:"loaf",p:["p2"]},{w:"Starter",p:["p3","p4"]},{w:"Tools",p:["p5"]},{w:"for",p:["p5"]},{w:"Enthusiasts",p:["p5"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8, 9, 10, 6, 11];
+ph["p3"]=[8, 9, 6, 12];
+ph["p4"]=[6, 12];
+ph["p5"]=[13, 14, 6, 15];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
